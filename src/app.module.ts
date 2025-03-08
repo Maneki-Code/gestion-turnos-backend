959d1223438500ee0d7dfb6e0ff58dac715c11d4
Rafael <rtoresani@gmail.com>
2025-03-08 16:08:30 -0300

    config global implemented

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { UsersModule } from './modules/users/users.module';
 import { ConfigModule } from '@nestjs/config';
 import databaseConfig from './config/database.config';
+import appConfig from './config/app.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: `${process.cwd()}/src/config/environments/.${process.env.NODE_ENV?.trim()}.env`,
-      load:[databaseConfig],
+      load:[appConfig, databaseConfig],
       isGlobal:true
     }),
     UsersModule],
