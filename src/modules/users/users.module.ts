8936d2d56ef18a898d83ef1ef7da2cbdca09f5e2
Rafael <rtoresani@gmail.com>
2025-03-08 23:03:13 -0300

    refactor: deleted imports on user module

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/config/database/prisma/prisma.service';
 import { UsersService } from './services/users.service';
 import { UsersController } from './controllers/users.controller';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
 
 @Module({
-  imports: [ConfigModule],
-  providers: [ConfigService, JwtService, PrismaService, UsersService],
+  providers: [JwtService, PrismaService, UsersService],
   controllers: [UsersController],
   exports: [UsersService]
 })
