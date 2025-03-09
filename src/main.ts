1a982f9f38d567a8068ea7fe9b95707126c62c69
Rafael <rtoresani@gmail.com>
2025-03-09 17:36:48 -0300

    add: standard success and error response

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ResponseInterceptor } from './common/interceptors/response.interceptor';
+import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,7 +14,8 @@ async function bootstrap() {
     whitelist: true
   }));
   app.setGlobalPrefix(`api/${configService.get('app.version')}`)
-  
+  app.useGlobalInterceptors(new ResponseInterceptor());
+  app.useGlobalFilters(new AllExceptionsFilter());
   const config = new DocumentBuilder()
     .setTitle('Gestión turnos API')
     .setDescription('')
