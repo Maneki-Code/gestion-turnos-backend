07f86d5ac14c402c8ad998f3127a71917e3d1cb6
Rafael <rtoresani@gmail.com>
2025-03-12 14:39:51 -0300

    add: update db

--- src/config/database/prisma/prisma.service.ts
+++ src/config/database/prisma/prisma.service.ts
@@ -3,13 +3,11 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  // Método que se ejecuta cuando el módulo se inicializa
   async onModuleInit() {
-    await this.$connect(); // Conectar a la base de datos
+    await this.$connect(); 
   }
 
-  // Método que se ejecuta cuando el módulo se destruye
   async onModuleDestroy() {
-    await this.$disconnect(); // Desconectar de la base de datos
+    await this.$disconnect(); 
   }
 }
