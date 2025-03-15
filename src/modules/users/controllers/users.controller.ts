3a4f141716a4ff8d37477c09db450a7fa0a894ef
Fran <delafuentefranco01@gmail.com>
2025-03-15 15:52:46 -0300

    Find user by id

--- src/modules/users/controllers/users.controller.ts
+++ src/modules/users/controllers/users.controller.ts
@@ -1,12 +1,17 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth } from '@nestjs/swagger';
 import { Roles } from 'src/common/decorators/roles.decorator';
 import { AuthGuard } from 'src/common/guards/auth.guard';
 import { RolesGuard } from 'src/common/guards/roles.guard';
+import { UsersService } from '../services/users.service';
 
 
 @Controller('users')
 export class UsersController {
+
+  constructor(private readonly _userServices:UsersService){
+
+  }
 
   @ApiBearerAuth() 
   @Get()
@@ -14,5 +19,11 @@ export class UsersController {
   /* @Roles('MANAGER') */
   helloFromSecured(){
     return "Hola desde un protegido";
+  }
+
+  
+  @Get('/:id')
+  findUserById(@Param('id')id:number){
+    return this._userServices.GetUserById(id)
   }
 }
