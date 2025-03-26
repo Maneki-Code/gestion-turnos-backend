c0f9e472c4469ab977ec453ee0581c445ed8f2d2
Rafael <rtoresani@gmail.com>
2025-03-26 14:39:57 -0300

    add: search customers

--- src/modules/customers/controllers/customers.controller.ts
+++ src/modules/customers/controllers/customers.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { CustomersService } from '../services/customers.service';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { AuthGuard } from 'src/common/guards/auth.guard';
+import { RolesGuard } from 'src/common/guards/roles.guard';
+import { CustomerResponse } from '../dtos/customer.response';
 
 @Controller('customers')
-export class CustomersController {}
+export class CustomersController {
+  constructor(private readonly _customerService:CustomersService){}
+
+  @UseGuards(AuthGuard, RolesGuard)
+  @Roles('ADMIN')
+  @Get()
+  async findAll(): Promise<CustomerResponse[]>{
+    return await this._customerService.findAll();
+  }
+
+  @Get('search')
+  async search(@Query('query') query: string): Promise<CustomerResponse[]> {
+    if (!query) return []; // Si no hay query, devolver lista vacía
+    return await this._customerService.searchCustomers(query);
+  }
+}
--- src/modules/customers/services/customers.service.ts
+++ src/modules/customers/services/customers.service.ts
@@ -6,17 +6,24 @@ import {
 import { Customer } from '@prisma/client';
 import { PrismaService } from 'src/config/database/prisma/prisma.service';
 import { CustomerForCreationDto } from '../dtos/customerForCreationDto.dto';
+import { CustomerResponse } from '../dtos/customer.response';
+import { CustomerMapperService } from 'src/common/mappers/services/customer-mapper.service';
 
 @Injectable()
 export class CustomersService {
-  constructor(private readonly _prisma: PrismaService) {}
+  constructor(
+    private readonly _prisma: PrismaService,
+    private readonly _customerMapper: CustomerMapperService,
+  ) {}
 
   async create(request: CustomerForCreationDto): Promise<Customer> {
     const customerFound = await this.findByPhoneNumber(request.phoneNumber);
-    
+
     if (customerFound !== null)
-      throw new ConflictException(`El número de teléfono ${request.phoneNumber} ya está asociado a un cliente`,);
-    
+      throw new ConflictException(
+        `El número de teléfono ${request.phoneNumber} ya está asociado a un cliente`,
+      );
+
     const customerCreated = await this._prisma.customer.create({
       data: {
         firstName: request.firstName,
@@ -39,4 +46,28 @@ export class CustomersService {
     });
   }
 
+  async findAll(): Promise<CustomerResponse[]> {
+    const customers = await this._prisma.customer.findMany({});
+
+    return customers.map((customer) =>
+      this._customerMapper.customerToFullResponse(customer),
+    );
+  }
+
+  async searchCustomers(query: string): Promise<CustomerResponse[]> {
+    const customers = await this._prisma.customer.findMany({
+      where: {
+        OR: [
+          { firstName: { contains: query } },
+          { lastName: { contains: query } },
+          { phoneNumber: { contains: query } },
+          { email: { contains: query } },
+        ],
+      },
+    });
+
+    return customers.map((customer) =>
+      this._customerMapper.customerToFullResponse(customer),
+    );
+  }
 }
