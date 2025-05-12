168c550a04824cb745490d67f994b6592e3c524c
Rafael <rtoresani@gmail.com>
2025-05-12 16:56:15 -0300

    add: relation user offer services

--- src/common/mappers/mappers.module.ts
+++ src/common/mappers/mappers.module.ts
@@ -3,9 +3,10 @@ import { AppointmentMapperService } from './services/appointment-mapper.service'
 import { CustomerMapperService } from './services/customer-mapper.service';
 import { ScheduleMapperService } from './services/schedule-mapper.service';
 import { UserMapperService } from './services/user-mapper.service';
+import { OfferedServicesMapperService } from './services/offered-services-mapper.service';
 
 @Module({
-  providers: [AppointmentMapperService, CustomerMapperService, ScheduleMapperService, UserMapperService],
+  providers: [AppointmentMapperService, CustomerMapperService, ScheduleMapperService, UserMapperService, OfferedServicesMapperService],
   exports: [AppointmentMapperService, CustomerMapperService, ScheduleMapperService]
 })
 export class MappersModule {}
